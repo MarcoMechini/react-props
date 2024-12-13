c0e1e9fa7353a07b26515fe890af49e823a655ab
Marco Mechini <mechinimarco35@gmail.com>
2024-12-13 17:29:50 +0100

    feat: milestone 2

--- src/components/Main/AppMain.jsx
+++ src/components/Main/AppMain.jsx
@@ -1,11 +1,19 @@
-import AppPost from '../Post/Post'
+import Post from '../Post/Post'
 import './AppMain.module.css'
+import arrayPost from '../../data/post'
+
+function setPost(array) {
+    return array.map(curPost => {
+        return (curPost.published && <Post key={curPost.id} post={curPost} />)
+    })
+}
+
 
 export default function AppMain() {
     return (
         <main>
             <h2>Il mio blog</h2>
-            <AppPost />
+            {setPost(arrayPost)}
         </main>
     )
 }
